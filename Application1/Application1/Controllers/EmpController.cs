1d70093c263876b6d2283b9193d661b73efbee37
Mahmoud Atia <mahmoudatia024@gmail.com>
2024-11-10 23:37:33 +0200

    First Files

---
+++ Application1/Application1/Controllers/EmpController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application1.Controllers
+{
+    public class EmpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Form1()
+        {
+            return View();
+        }
+
+        public IActionResult Form2()
+        {
+            return View();
+        }
+
+
+        public string print() {
+            return "Hello Mahmoud";
+        }
+
+        public int sum(int a , int b) { 
+            return a + b;
+        }
+    }
+}
